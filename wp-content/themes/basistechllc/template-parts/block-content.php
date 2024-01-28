6c94819f3335f1cada82a38cba1cd7bc17df2daf
Sunny <sunnywz@gmail.com>
2024-01-28 17:03:30 -0600

    Update block-content.php

--- wp-content/themes/basistechllc/template-parts/block-content.php
+++ wp-content/themes/basistechllc/template-parts/block-content.php
@@ -1,6 +1,8 @@
-				<?php the_sub_field( 'block_basics_background_color' ); ?>
-				<?php the_sub_field( 'block_basics_custom_classes' ); ?>
-				<?php the_sub_field( 'block_basics_section_anchor' ); ?>
-				<?php the_sub_field( 'cb_section_title' ); ?>
-				<?php the_sub_field( 'cb_display_mode' ); ?>
-				<?php the_sub_field( 'mobile_order' ); ?>
+			<?php the_sub_field( 'block_basics_background_color' ); ?>
+			<?php the_sub_field( 'block_basics_custom_classes' ); ?>
+			<?php the_sub_field( 'block_basics_section_anchor' ); ?>
+			<?php the_sub_field( 'cb_section_title' ); ?>
+			<?php the_sub_field( 'cb_display_mode' ); ?>
+			<?php the_sub_field( 'cb_mobile_order' ); ?>
+			<?php the_sub_field( 'cb_content' ); ?>
+			<?php the_sub_field( 'cb_content_column_2' ); ?>
